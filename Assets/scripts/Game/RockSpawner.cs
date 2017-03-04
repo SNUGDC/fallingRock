8aacd38e8f677097551e374d1a67ae7a1ee5dd62
Juhyeong Park <majecty@gmail.com>
2017-03-05 06:56:17 +0900

    Make rock count similar

--- Assets/scripts/Game/RockSpawner.cs
+++ Assets/scripts/Game/RockSpawner.cs
@@ -27,30 +27,56 @@ public class RockSpawner : GameOverOutput
     {
         while (!stop)
         {
-			foreach (var sSpawnPoint in sSpawnPoints)
-			{
-				var rock = Spawn(sSpawnPoint);
+            var randomPrefabs = RandomSequence();
+            for (int i = 0; i < sSpawnPoints.Count; i++)
+            {
+                var rock = Spawn(sSpawnPoints[i], randomPrefabs[i]);
                 output.OnSpawn(rock);
-			}
+            }
 			var spawnCool = Configurations.Instance.rockDownCool;
             yield return new WaitForSeconds(spawnCool);
         }
     }
 
-	private Rock Spawn(SpawnPoint sSpawnPoint) {
-		Rock randomPrefab;
-		if (UnityEngine.Random.Range(0f, 1f) < 0.5f) {
-			randomPrefab = rock1Prefab;
-		} else {
-			randomPrefab = rock2Prefab;
-		}
-
+	private Rock Spawn(SpawnPoint sSpawnPoint, Rock randomPrefab) {
 		Rock newRock = GameObject.Instantiate(randomPrefab);
 		newRock.transform.SetParent(sSpawnPoint.transform, false);
         newRock.sSpawnPoint = sSpawnPoint;
 
 		return newRock;
 	}
+
+    private List<Rock> RandomSequence() {
+        var values = new List<Rock>();
+        values.Add(rock1Prefab);
+        values.Add(rock1Prefab);
+        values.Add(rock2Prefab);
+        values.Add(rock2Prefab);
+        if (UnityEngine.Random.Range(0f, 1f) < 0.5f) {
+			values.Add(rock1Prefab);
+		} else {
+			values.Add(rock2Prefab);
+		}
+        if (UnityEngine.Random.Range(0f, 1f) < 0.5f) {
+			values.Add(rock1Prefab);
+		} else {
+			values.Add(rock2Prefab);
+		}
+        return Shuffle(values);
+    }
+
+    private List<Rock> Shuffle(List<Rock> values)
+    {
+        for (int i = 0; i < values.Count; i++)
+        {
+            var otherI = UnityEngine.Random.Range(0, values.Count);
+            Rock tmp = values[i];
+            values[i] = values[otherI];
+            values[otherI] = tmp;
+        }
+
+        return values;
+    }
 
     void GameOverOutput.OnGameEnd(Player loser)
     {
