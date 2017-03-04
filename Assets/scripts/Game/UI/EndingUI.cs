1fac0a7f69c776f7af072044f437ef9453410ce0
Juhyeong Park <majecty@gmail.com>
2017-03-05 06:59:58 +0900

    Make regame easy

--- Assets/scripts/Game/UI/EndingUI.cs
+++ Assets/scripts/Game/UI/EndingUI.cs
@@ -7,14 +7,33 @@ public class EndingUI : MonoBehaviour, GameOverOutput {
 	public PlayerColor color;
 	[SerializeField]
 	private Image image;
+	bool gameEnd = false;
 
 	public void OnTouchToMain()
 	{
 		SceneManager.LoadScene("main");
 	}
 
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if (!gameEnd) {
+			return;
+		}
+
+		if (Input.GetKeyUp(KeyCode.Escape) ||
+			Input.GetKeyUp(KeyCode.Space) ||
+			Input.GetKeyUp(KeyCode.Return) ||
+			Input.GetKeyUp(KeyCode.Backspace)) {
+			SceneManager.LoadScene("main");
+		}
+	}
+
     void GameOverOutput.OnGameEnd(Player loser)
     {
+		gameEnd = true;
 		if (loser.color == color) {
         	image.enabled = true;
 		}
