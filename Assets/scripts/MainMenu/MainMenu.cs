b74678a6ff7730c286827f1e4f6fc281a8398c36
Juhyeong Park <majecty@gmail.com>
2017-03-05 00:32:56 +0900

    Add game scene

--- Assets/scripts/MainMenu/MainMenu.cs
+++ Assets/scripts/MainMenu/MainMenu.cs
@@ -1,16 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void OnStartButtonClicked() {
+		Debug.Log("Start button click");
+		SceneManager.LoadScene("game");
 	}
 }
